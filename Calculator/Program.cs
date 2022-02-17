2d1e81365a0a15130cf2a8cc63c0349e84dae1ea
Tesfahun Yemisrach <ybehope@gmail.com>
2022-02-17 15:45:17 -0800

    ++ Square root and power functionality added!!

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,39 +1,9 @@
 ﻿using System;
+using CalculatorProgram;
 
-namespace Calculator
+namespace  CalculatorProgram
 {
-    class Calculator
-    {
-        public static double DoOperation(double num1, double num2, string op)
-        {
-            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-            // Use a switch statement to do the math.
-            switch (op)
-            {
-                case "a":
-                    result = num1 + num2;
-                    break;
-                case "s":
-                    result = num1 - num2;
-                    break;
-                case "m":
-                    result = num1 * num2;
-                    break;
-                case "d":
-                    // Ask the user to enter a non-zero divisor.
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                    }
-                    break;
-                // Return text for an incorrect option entry.
-                default:
-                    break;
-            }
-            return result;
-        }
-    }
+   
     class Program
     {
         static void Main(string[] args)
@@ -43,6 +13,7 @@ namespace Calculator
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
 
+            Calculator calculator = new Calculator();
             while (!endApp)
             {
                 // Declare variables and set to empty.
@@ -78,13 +49,16 @@ namespace Calculator
                 Console.WriteLine("\ts - Subtract");
                 Console.WriteLine("\tm - Multiply");
                 Console.WriteLine("\td - Divide");
+                Console.WriteLine("\tsr - Square Root");
+                Console.WriteLine("\tpr - Percentile");
+                Console.WriteLine("\tpw - Power");
                 Console.Write("Your option? ");
 
                 string op = Console.ReadLine();
 
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -99,11 +73,12 @@ namespace Calculator
                 Console.WriteLine("------------------------\n");
 
                 // Wait for the user to respond before closing.
-                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-                if (Console.ReadLine() == "n") endApp = true;
+                Console.Write("Press 'q' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "q") endApp = true;
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
+            calculator.Finish();
             return;
         }
     }
